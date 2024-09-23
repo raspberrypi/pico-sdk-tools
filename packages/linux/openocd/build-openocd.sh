4a38aed566bc9b89cf94700c3e4f711d88626b87
William Vinnicombe <william.vinnicombe@raspberrypi.com>
2024-09-23 13:48:05 +0100

    Pass -static flags as env vars

--- packages/linux/openocd/build-openocd.sh
+++ packages/linux/openocd/build-openocd.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 cd openocd
 sed -i -e 's/uint /unsigned int /g' ./src/flash/nor/rp2040.c
 ./bootstrap
-./configure CFLAGS=-static LDFLAGS=-static --disable-werror
+./configure --disable-werror
 make clean
 make -j$(nproc)
 INSTALLDIR="$PWD/../openocd-install/usr/local/bin"
