aff8a658cea1446d467c44f704bc0448541c7ddb
Nikhil Dabas <nikhil@nikhildabas.com>
2024-12-13 21:52:02 +0000

    Path to GCC src needs to be relative

--- packages/windows/riscv/build-riscv-gcc.sh
+++ packages/windows/riscv/build-riscv-gcc.sh
@@ -9,7 +9,7 @@ mkdir -p $INSTALLDIR
 BUILDDIR=$(pwd)
 
 cd riscv-gnu-toolchain
-./configure --prefix=$BUILDDIR/$INSTALLDIR --with-arch=rv32ima_zicsr_zifencei_zba_zbb_zbs_zbkb_zca_zcb --with-abi=ilp32 --with-multilib-generator="rv32ima_zicsr_zifencei_zba_zbb_zbs_zbkb_zca_zcb-ilp32--;rv32imac_zicsr_zifencei_zba_zbb_zbs_zbkb-ilp32--" --with-gcc-src=$BUILDDIR/gcc
+./configure --prefix=$BUILDDIR/$INSTALLDIR --with-arch=rv32ima_zicsr_zifencei_zba_zbb_zbs_zbkb_zca_zcb --with-abi=ilp32 --with-multilib-generator="rv32ima_zicsr_zifencei_zba_zbb_zbs_zbkb_zca_zcb-ilp32--;rv32imac_zicsr_zifencei_zba_zbb_zbs_zbkb-ilp32--" --with-gcc-src=../gcc
 make -j$(nproc)
 
 cd "$BUILDDIR/$INSTALLDIR"
