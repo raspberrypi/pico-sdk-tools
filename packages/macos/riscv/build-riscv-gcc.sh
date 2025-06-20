2774b73af7e3cc4bbc118a0d4c869fe5eccd2b54
William Vinnicombe <william.vinnicombe@raspberrypi.com>
2025-06-20 16:51:23 +0100

    Remove separate GCC clone, and fix risc-v toolchain version

--- packages/macos/riscv/build-riscv-gcc.sh
+++ packages/macos/riscv/build-riscv-gcc.sh
@@ -13,7 +13,7 @@ GDB_TARGET_FLAGS_EXTRA="--with-gmp=/opt/homebrew --with-mpfr=/opt/homebrew"
 export GDB_TARGET_FLAGS_EXTRA
 
 cd riscv-gnu-toolchain
-./configure --prefix=$BUILDDIR/$INSTALLDIR --with-arch=rv32ima_zicsr_zifencei_zba_zbb_zbs_zbkb_zca_zcb --with-abi=ilp32 --with-multilib-generator="rv32ima_zicsr_zifencei_zba_zbb_zbs_zbkb_zca_zcb-ilp32--;rv32imac_zicsr_zifencei_zba_zbb_zbs_zbkb-ilp32--" --with-gcc-src=$BUILDDIR/gcc
+./configure --prefix=$BUILDDIR/$INSTALLDIR --with-arch=rv32ima_zicsr_zifencei_zba_zbb_zbs_zbkb_zca_zcb --with-abi=ilp32 --with-multilib-generator="rv32ima_zicsr_zifencei_zba_zbb_zbs_zbkb_zca_zcb-ilp32--;rv32imac_zicsr_zifencei_zba_zbb_zbs_zbkb-ilp32--"
 # 4 threads, as 8 threads runs out of memory
 gmake -j4
 
