e4bd285c09e28d7e74e6335d4914f281f7dc9fc1
William Vinnicombe <william.vinnicombe@raspberrypi.com>
2025-09-05 17:30:28 +0100

    Copy libjim and libgpiod .so files, and set rpath
    This should fix raspberrypi/pico-vscode#203 and #19

--- packages/linux/openocd/build-openocd.sh
+++ packages/linux/openocd/build-openocd.sh
@@ -13,3 +13,9 @@ make
 INSTALLDIR="$PWD/../openocd-install/usr/local/bin"
 rm -rf "$PWD/../openocd-install"
 DESTDIR="$PWD/../openocd-install" make install
+
+# Add libraries that may be different versions on the system
+cd $INSTALLDIR
+cp $(ldd openocd | egrep -o "(/.*/libgpiod\.so\.\S*)") ./
+cp $(ldd openocd | egrep -o "(/.*/libjim\.so\.\S*)") ./
+patchelf --set-rpath . openocd
