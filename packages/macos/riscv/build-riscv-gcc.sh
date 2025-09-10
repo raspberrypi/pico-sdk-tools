00821b21f6c8330285c3d723c0e2cd5c8d1c1b45
William Vinnicombe <william.vinnicombe@raspberrypi.com>
2025-09-10 14:19:34 +0100

    Attempt build of x64 on arm64 runner

--- packages/macos/riscv/build-riscv-gcc.sh
+++ packages/macos/riscv/build-riscv-gcc.sh
@@ -8,8 +8,10 @@ mkdir -p $INSTALLDIR
 
 BUILDDIR=$(pwd)
 
-GDB_TARGET_FLAGS_EXTRA="--with-gmp=/opt/homebrew --with-mpfr=/opt/homebrew"
-export GDB_TARGET_FLAGS_EXTRA
+if [[ $(uname -m) == 'arm64' ]]; then
+    GDB_TARGET_FLAGS_EXTRA="--with-gmp=/opt/homebrew --with-mpfr=/opt/homebrew"
+    export GDB_TARGET_FLAGS_EXTRA
+fi
 
 cd riscv-gnu-toolchain
 ./configure --prefix=$BUILDDIR/$INSTALLDIR --enable-strip --with-arch=rv32ima_zicsr_zifencei_zba_zbb_zbs_zbkb_zca_zcb --with-abi=ilp32 --with-multilib-generator="rv32ima_zicsr_zifencei_zba_zbb_zbs_zbkb_zca_zcb-ilp32--;rv32imac_zicsr_zifencei_zba_zbb_zbs_zbkb-ilp32--"
